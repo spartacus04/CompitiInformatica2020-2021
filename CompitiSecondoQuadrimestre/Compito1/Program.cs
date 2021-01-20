b825cb7c9410570a37998b3ad2672170bbcb7110
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-01-20 18:05:10 +0100

    Aggiungi i compiti del secondo quadrimestre

---
+++ CompitiSecondoQuadrimestre/Compito1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Compito1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Inserisci il numero di numeri: ");
+            do
+            {
+                n = int.Parse(Console.ReadLine());
+            } while (n <= 0);
+
+            double somma = 0, int i = 0;
+
+            do
+            {
+                Console.WriteLine("Inserisci un numero: ");
+                double num = double.Parse(Console.ReadLine());
+
+                if(num > 0){
+                    somma += num;
+                }
+
+                i++;
+            } while (i < n);
+
+            double media = somma / n;
+
+            Console.WriteLine("La media è " + media);
+            Console.ReadKey();
+
+        }
+    }
+}
