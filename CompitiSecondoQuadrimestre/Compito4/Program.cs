fea7febfb3de4ed257f5e06f014d550960a3e6d9
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-01-26 12:42:13 +0100

    aggiungo compito4

---
+++ CompitiSecondoQuadrimestre/Compito4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Compito4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, somma = 0;
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Inserisci il primo numero: ");
+                a = int.Parse(Console.ReadLine());
+            } while (a < 0);
+
+            do
+            {
+                Console.WriteLine("Inserisci il secondo numero: ");
+                b = int.Parse(Console.ReadLine());
+            } while (b < 0);
+
+
+            do
+            {
+                somma += a;
+                i++;
+            } while (i < b);
+
+            Console.WriteLine("Il risultato è " + somma);
+        }
+    }
+}
