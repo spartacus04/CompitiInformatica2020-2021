85f4bc384cc7b8dbf3a18a7a5320cdbc82b8edf6
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-01-28 17:15:14 +0100

    Aggiungo compito6

---
+++ CompitiSecondoQuadrimestre/Compito6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Compito6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int i = 0, j = 1;
+            char s;
+
+            do
+            {
+                int somma = 0;
+                Console.WriteLine("Inserisci il primo numero: ");
+                a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Inserisci il secondo numero: ");
+                b = int.Parse(Console.ReadLine());
+
+                do
+                {
+                    somma += a;
+                    i++;
+                } while (i < b);
+
+                Console.WriteLine("Il prodotto della coppia " + j + " è uguale a " + somma);
+
+                Console.WriteLine("Vuoi inserire altri numeri? (Si/No)");
+
+                s = char.Parse(Console.ReadLine());
+                j++;
+            } while (s == 's' || s == 'S');
+        }
+    }
+}
