7480dfc8f1f06f48c324d7364016ac869338af8d
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-01-28 17:38:43 +0100

    Aggiungo Compito7

---
+++ CompitiSecondoQuadrimestre/Compito7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Compito7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int somma = 0;
+
+            do
+            {
+                Console.WriteLine("Inserisci un numero: ");
+                int a = int.Parse(Console.ReadLine());
+
+                somma += a;
+                if(a % 2 == 0)
+                    i++;
+            } while (i < 5);
+
+            Console.WriteLine("La somma è uguale a " + somma);
+            Console.ReadKey();
+        }
+    }
+}
