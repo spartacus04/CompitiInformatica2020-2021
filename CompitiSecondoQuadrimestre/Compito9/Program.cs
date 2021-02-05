9c936c002544de6d187c724b1a0043b9dcb3abe0
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-02-05 11:52:18 +0100

    aggiungo compito9

---
+++ CompitiSecondoQuadrimestre/Compito9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Compito9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il primo numero: ");
+            int a = int.Parse(Console.ReadLine());
+
+            int b;
+            do
+            {
+                Console.WriteLine("Inserire il secondo numero:" );
+                b = int.Parse(Console.ReadLine());
+            } while (b <= 0);
+
+            int i = 0;  //Cont
+            
+            while(i < b){
+                int num;
+                do
+                {
+                    Console.WriteLine("Inserire un numero minore di " + a + ": ");
+                    num = int.Parse(Console.ReadLine());
+                } while (num >= a);
+                i++;
+            }
+
+            Console.WriteLine("Sono stati inseriti " + b + " numeri < di " + a);
+            Console.ReadKey();
+        }
+    }
+}
