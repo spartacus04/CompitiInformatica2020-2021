7120b79f2d4233a314e8da3ad593a144c0c3f8c4
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-02-05 12:14:23 +0100

    aggiungo compito10

---
+++ CompitiSecondoQuadrimestre/Compito10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Compito10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, j = 1, somma = 0;
+            do
+            {
+                Console.WriteLine("Inserisci un numero: ");
+                a = int.Parse(Console.ReadLine());
+            } while (a <= 0);
+
+            for (int i = 0; i < a; i++)
+            {
+                somma += j;
+                j += 2;
+            }
+
+            j = 1;
+            somma = 0;
+            int ii = 0;
+            do
+            {
+                somma += j;
+                j += 2;
+                ii++;
+            } while (ii < a);
+
+            Console.WriteLine("Il quadrato di " + a + " è " + somma);
+        }
+    }
+}
