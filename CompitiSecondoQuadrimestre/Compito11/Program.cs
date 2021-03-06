e2e53154c707efc19a57065e0e403aa1400a92df
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-03-06 17:02:41 +0100

    aggiungo compito11

---
+++ CompitiSecondoQuadrimestre/Compito11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Compito11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            do
+            {
+                Console.WriteLine("Inserisci il numero di film: ");
+                N = int.Parse(Console.ReadLine());
+            } while (N <=0);
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Inserisci il titolo del film: ");
+                string titolo = Console.ReadLine();
+                
+                int somma = 0;
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("Inserisci il numero di spettatori nel giorno " + j);
+                    int spettatori = int.Parse(Console.ReadLine());
+
+                    somma += spettatori;    //somma = somma + spettatori
+                }
+
+                double media = somma / 10;
+                Console.WriteLine("Il film " + titolo + " ha avuto " + media + " visualizzazioni medie");
+            }
+            Console.ReadKey();
+        }
+    }
+}
