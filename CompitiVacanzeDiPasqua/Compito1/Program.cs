ed3bbd6013b0beedc891bead2b93e4e1bd2d680a
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-03-31 16:24:59 +0200

    aggiungo compiti delle vacanze di pasqua

---
+++ CompitiVacanzeDiPasqua/Compito1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Compito1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int casse = input("Inserisci il numero di casse");
+            double media = 0;
+
+            for (int i = 0; i < casse; i++)
+            {
+                int peso = input("Inserisci il peso della cassa numero " + (i + 1));
+                int dist = input("Inserisci la distanza di spedizione della cassa numero " + (i + 1));
+
+                int cost = costo(peso, dist);
+                media += cost;
+
+                Console.WriteLine("Il costo della cassa numero " + (i + 1) + " è uguale a " + cost + " euro\n");
+            }
+
+            media /= casse;
+
+            Console.WriteLine("Il costo medio delle casse è " + Math.Round(media, 2) + " euro");
+            Console.ReadKey();
+        }
+
+        static int input(string messaggio){
+            int a;
+            do
+            {
+                Console.WriteLine(messaggio);
+                a = int.Parse(Console.ReadLine());
+            } while (a <= 0);
+            return a;
+        }
+
+        static int costo(int peso, int dist){
+
+            int costoDistanza;
+            if(dist <= 50){
+               costoDistanza = 5;
+            }
+            else if(dist > 50 && dist <= 100){
+                costoDistanza = 10;
+            }
+            else if(dist > 100 && dist <= 300){
+                costoDistanza = 14;
+            }
+            else{
+                costoDistanza = 20;
+            }
+            
+            return costoDistanza + (peso * 3);
+        }
+    }
+}
---
+++ CompitiVacanzeDiPasqua/Compito2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Compito2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lav = input("Inserisci il numero di lavoratori");
+            double trattenuteTotali = 0;
+
+            for (int i = 0; i < lav; i++)
+            {
+                int cost = input("Inserisci il costo di un ora di lavoro del lavoratore numero " + (i + 1));
+                int ore = input("Inserisci il numero di ore di lavoro del lavoratore numero " + (i + 1));
+
+                double costoTotale = cost * ore;
+                double trattenuta = Math.Round(trattenute(costoTotale));
+                trattenuteTotali += trattenuta;
+                costoTotale -= trattenuta;
+
+                Console.WriteLine("Il lavoratore numero " + (i + 1) + " verrà pagato " + costoTotale + " euro\n");
+            }
+
+            Console.WriteLine("Le trattenute totali sono di " + trattenuteTotali + " euro");
+            Console.ReadKey();
+        }
+
+        static int input(string messaggio){
+            int a;
+            do
+            {
+                Console.WriteLine(messaggio);
+                a = int.Parse(Console.ReadLine());
+            } while (a <= 0);
+            return a;
+        }
+
+        static double trattenute(double costoTotale){
+            return (costoTotale / 100) * 27;
+        }
+        
+    }
+}
