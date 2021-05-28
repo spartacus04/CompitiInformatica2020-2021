823ca9265546c68558f8ad201a4a98d0732a461a
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-05-28 11:55:41 +0200

    Aggiungo compito17, 18, 19

---
+++ CompitiSecondoQuadrimestre/Compito17/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Compito17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = randomVect();
+            int[] b = randomVect();
+            int[] c = new int[100];
+            int tot = 0;
+
+            double med = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i] > b[i]) c[i] = a[i];
+                else c[i] = b[i];
+
+                med += c[i];
+            }
+
+            med /= c.Length;
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if(c[i] > med) tot++;
+            }
+
+            System.Console.WriteLine("La media del vettore c è " + med);
+            System.Console.WriteLine("Ci sono " + tot + " numeri sopra la media");
+            Console.ReadKey();
+        }
+
+
+        static int[] randomVect(){
+            Random rand = new Random();
+            int[] vect = new int[100];
+            for (int i = 0; i < vect.Length; i++)
+            {
+                vect[i] = rand.Next(0, 101);
+            }
+
+            return vect;
+        }
+    }
+}
---
+++ CompitiSecondoQuadrimestre/Compito18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Compito18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mesi = {"gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"};
+            int[] max = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            int g = input("Inserisci il giorno");
+            int m = input("Inserisci il mese (1-12)");
+
+            if(m <= 12 && g <= max[m - 1])  Console.WriteLine("La data " + g + " "+ mesi[m-1] + " è valida");
+            else Console.WriteLine("Data non valida");
+            
+            Console.ReadKey();
+        }
+
+        static int input(string msg){
+            int a;
+            do
+            {
+                System.Console.WriteLine(msg);
+                a = int.Parse(Console.ReadLine());
+            } while (a < 0);
+            return a;
+        }
+    }
+}
